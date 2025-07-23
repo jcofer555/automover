174aad7cafec0d1f1da26c0323284d15a4ba22a9
jcofer555 <140566618+jcofer555@users.noreply.github.com>
2025-07-23 01:58:15 -0700

    Update mover_wrapper.sh

--- src/usr/local/emhttp/plugins/automover/helpers/mover_wrapper.sh
+++ src/usr/local/emhttp/plugins/automover/helpers/mover_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 logger "🔄 Automover Wrapper: Starting mover"
 rm -f /var/log/mover_debug.log
-/usr/local/sbin/mover start | tee -a /var/log/mover_debug.log
+/usr/local/sbin/mover start | tee -a /var/log/automover_files_moved.log
 logger "✅ Automover Wrapper: Mover finished"
